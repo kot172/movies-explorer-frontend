dd57eaba427fa4a397758a135003b7409fa8e4e4
Nikita Bogomazov <Bnikita72@yandex.ru>
2023-11-20 22:05:25 +0500

    Refactoring

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,21 +1,41 @@
-import './Portfolio.css'
+import "./Portfolio.css";
 
 export default function Portfolio() {
-
-    return(
-        <section className='portfolio'>
-<p className='portfolio__caption'>Портфолио</p>
-<ul className='portfolio__link'>
-<li className='portfolio__link-caption'>
-       Статичный сайт
-       <a className='portfolio__link-btn' target='_blank' href='https://github.com/kot172/how-to-learn'>
-        </a></li>
-<li className='portfolio__link-caption'>
-    Адаптивный сайт <a className='portfolio__link-btn' target='_blank' href='https://github.com/kot172/russian-travel'>
-        </a></li>
-<li className='portfolio__link-caption'>
-    Одностраничное приложение <a className='portfolio__link-btn' target='_blank' href='https://github.com/kot172/react-mesto-auth'>
-        </a></li></ul>
-        </section>
-    )
+  return (
+    <section className="portfolio">
+      <p className="portfolio__caption">Портфолио</p>
+      <ul className="portfolio__link">
+        <li className="portfolio__link-caption">
+          <a
+            className="portfolio__link-btn"
+            target="_blank"
+            href="https://github.com/kot172/how-to-learn"
+          >
+            <p className="portfolio__link-text">Статичный сайт</p>
+            <button type="button" className="portfolio__link-cursor"></button>
+          </a>
+        </li>
+        <li className="portfolio__link-caption">
+          <a
+            className="portfolio__link-btn"
+            target="_blank"
+            href="https://github.com/kot172/russian-travel"
+          >
+            <p className="portfolio__link-text">Адаптивный сайт</p>
+            <button type="button" className="portfolio__link-cursor"></button>
+          </a>
+        </li>
+        <li className="portfolio__link-caption">
+          <a
+            className="portfolio__link-btn"
+            target="_blank"
+            href="https://github.com/kot172/react-mesto-auth"
+          >
+            <p className="portfolio__link-text">Одностраничное приложение</p>
+            <button type="button" className="portfolio__link-cursor"></button>
+          </a>
+        </li>
+      </ul>
+    </section>
+  );
 }
